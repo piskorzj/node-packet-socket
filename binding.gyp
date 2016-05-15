ad9f6b29156d056fe882be51126ea1c69b3644a3
Jakub Piskorz <piskorzj@gmail.com>
2016-05-15 21:58:02 +0200

    Add initial unit tests for node addon module

--- binding.gyp
+++ binding.gyp
@@ -10,6 +10,17 @@
       "cflags!": [ "-fno-exceptions" ],
       "cflags_cc!": [ "-fno-exceptions" ]
     },
+    {
+      "target_name": "test_wrapper",
+      "sources": [ "src/wrapper.cc", "cc_mocks/socket.cc", "cc_tests/wrapper.cc" ],
+      "include_dirs": [
+          "<!(node -e \"require('nan')\")",
+          "include"
+      ],
+      "libraries": [ "-lCppUTest", "-lCppUTestExt"  ],
+      "cflags!": [ "-fno-exceptions" ],
+      "cflags_cc!": [ "-fno-exceptions" ]
+    }
   ]
 }
 
---
+++ cc_tests/wrapper.cc
@@ -0,0 +1,34 @@
+#include "wrapper.hh"
+
+#include "CppUTest/TestHarness.h"
+#include "CppUTest/CommandLineTestRunner.h"
+#include "CppUTestExt/MockSupport.h"
+
+TEST_GROUP(WrapperInitGroup) {
+	void teardown() {
+		mock().checkExpectations();
+		mock().clear();
+	}
+};
+
+NAN_METHOD(Run) {
+	int argc = info.Length();
+	char ** argv = new char*[argc];
+	for(int i = 0; i < argc; i++) {
+		int arg_len = Nan::To<v8::String>(info[i]).ToLocalChecked()->Utf8Length();
+		argv[i] = new char[arg_len];
+		Nan::To<v8::String>(info[i]).ToLocalChecked()->WriteUtf8(argv[i]);
+	}
+	MemoryLeakWarningPlugin::turnOffNewDeleteOverloads();
+	int rc = CommandLineTestRunner::RunAllTests(argc, argv);
+	for(int i = 0; i < argc; i++)
+		delete [] argv[i];
+	delete [] argv;
+	info.GetReturnValue().Set(rc);
+}
+
+NAN_MODULE_INIT(TestRunner) {
+	Nan::SetMethod(target, "Run", Run);
+}
+
+NODE_MODULE(test, TestRunner);
