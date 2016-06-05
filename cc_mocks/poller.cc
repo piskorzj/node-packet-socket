dbf335262c6a2e1bfd31b3177d4327cb905dda59
Jakub Piskorz <piskorzj@gmail.com>
2016-06-05 02:07:15 +0200

    Add reduced poller mocks

--- cc_mocks/poller.cc
+++ cc_mocks/poller.cc
@@ -1,2 +1,23 @@
 #include "CppUTestExt/MockSupport.h"
 #include "poller.hh"
+
+
+Poller::Poller(
+  int descriptor,
+  void (*read_ready_callback)(void *data),
+  void (*write_ready_callback)(void *data),
+  void (*error_callback)(void *data, const char *error),
+  void *external_data
+) {}
+
+Poller::~Poller(void) {}
+
+void Poller::on_close(uv_handle_t *handle) {}
+
+void Poller::io_event_wrapper(uv_poll_t *watcher, int status, int revents) {}
+
+void Poller::io_event(int status, int revents) {}
+
+void Poller::set_events(PollerEvents events) {
+  mock().actualCall("set_events").withIntParameter("events", events);
+}
