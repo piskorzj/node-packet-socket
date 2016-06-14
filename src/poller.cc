1f89f0a2cfea7da1201c918aadbd9f40953b28de
Jakub Piskorz <piskorzj@gmail.com>
2016-06-14 21:58:31 +0200

    Add Poller abstract and rename Poller to UvPoller

--- src/poller.cc
+++ src/poller.cc
@@ -1,6 +1,6 @@
 #include "poller.hh"
 
-Poller::Poller(
+UvPoller::UvPoller(
   int descriptor,
   void (*read_ready_callback)(void *data),
   void (*write_ready_callback)(void *data),
@@ -14,23 +14,23 @@ Poller::Poller(
   uv_poll_init_socket(uv_default_loop(), uv_poll, descriptor);
   uv_poll->data = this;
   current_events = READ_EVENT;
-  uv_poll_start(uv_poll, UV_READABLE, Poller::io_event_wrapper);
+  uv_poll_start(uv_poll, UV_READABLE, UvPoller::io_event_wrapper);
 }
 
-Poller::~Poller(void) {
-  uv_close(reinterpret_cast<uv_handle_t *>(uv_poll), Poller::on_close);
+UvPoller::~UvPoller(void) {
+  uv_close(reinterpret_cast<uv_handle_t *>(uv_poll), UvPoller::on_close);
 }
 
-void Poller::on_close(uv_handle_t *handle) {
+void UvPoller::on_close(uv_handle_t *handle) {
   delete handle;
 }
 
-void Poller::io_event_wrapper(uv_poll_t *watcher, int status, int revents) {
-  Poller *poller = static_cast<Poller *>(watcher->data);
+void UvPoller::io_event_wrapper(uv_poll_t *watcher, int status, int revents) {
+  UvPoller *poller = static_cast<UvPoller *>(watcher->data);
   poller->io_event(status, revents);
 }
 
-void Poller::io_event(int status, int revents) {
+void UvPoller::io_event(int status, int revents) {
   if(status) {
     error_callback(external_data, uv_strerror(-status));
   } else {
@@ -41,10 +41,10 @@ void Poller::io_event(int status, int revents) {
   }
 }
 
-void Poller::set_events(PollerEvents events) {
+void UvPoller::set_events(PollerEvents events) {
   if(events == current_events) return;
   current_events = events;
   uv_poll_stop(uv_poll);
   if(events)
-    uv_poll_start(uv_poll, events, Poller::io_event_wrapper);
+    uv_poll_start(uv_poll, events, UvPoller::io_event_wrapper);
 }
